f9609d44e4c4442a857a41ab5eb624bd7891d7a0
Anderson <andchavescruz@gmail.com>
2026-03-31 11:13:11 -0300

    Positivo e Negstivo Nesse caso tive que usar ELSE IF, porque poderia dar três condições

---
+++ positivo_negativo/positivo_negativo.js
@@ -0,0 +1,8 @@
+let num = -10
+if (num > 0) {
+    console.log(`Número ${num} é POSITIVO!`)
+} else if (num == 0) {
+    console.log(`Número ${num} é NEUTRO!`)
+} else {
+    console.log(`Número ${num} é NEGATIVO!`)
+}
