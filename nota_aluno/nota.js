5b59c9f3e5f56c0e1d2aa2aea802e20d2471673a
Anderson <andchavescruz@gmail.com>
2026-03-31 11:51:42 -0300

    Verifica nota do aluno.

---
+++ nota_aluno/nota.js
@@ -0,0 +1,8 @@
+let nota = 6.8
+if (nota >= 7.0) {
+    console.log(`Você foi tirou ${nota} aprovado!`)
+} else if (nota < 6.0) {
+    console.log(`Você foi tirou ${nota} reprovado!`)
+} else {
+    console.log(`Você tirou ${nota} esta de recuperação!`)
+}
