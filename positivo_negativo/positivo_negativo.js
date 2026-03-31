b97f9cd5abb368d9152c8df2a659a27ce62396ac
Anderson <andchavescruz@gmail.com>
2026-03-31 11:16:36 -0300

    SIMPLIFICADO

--- positivo_negativo/positivo_negativo.js
+++ positivo_negativo/positivo_negativo.js
@@ -1,8 +1,8 @@
 let num = -10
 if (num > 0) {
     console.log(`Número ${num} é POSITIVO!`)
-} else if (num == 0) {
-    console.log(`Número ${num} é NEUTRO!`)
-} else {
+} else if (num < 0) {
     console.log(`Número ${num} é NEGATIVO!`)
+} else {
+    console.log(`Número ${num} é NEUTRO!`)
 }
