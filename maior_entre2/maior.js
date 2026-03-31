6855fb92c7e9f05f7b2e4c7174ed68507f02d95e
Anderson <andchavescruz@gmail.com>
2026-03-31 11:30:58 -0300

    MAIOR ENTRE DOIS

---
+++ maior_entre2/maior.js
@@ -0,0 +1,9 @@
+let num1 = -10
+let num2 = 4
+if (num1 > num2){
+    console.log(`O primeiro número é ${num1} ele é maior que ${num2}`)
+} else if (num1 < num2) {
+    console.log(`O segundo número é ${num2} ele é maior que ${num1}`)
+} else {
+    console.log('Os números são iguais!')
+}
