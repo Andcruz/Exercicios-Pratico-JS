f9df0f770652ed2460c74432c456189b0c9573a2
Anderson <andchavescruz@gmail.com>
2026-04-10 13:23:11 -0300

    contagem regressíva de forma reduzida.

--- EstruturaRepetição/estruturaFor/node_06756eec2631b.tmp.js
+++ EstruturaRepetição/estruturaFor/node_06756eec2631b.tmp.js
@@ -5,8 +5,8 @@ for (i = 0; i <= 10; i++) {
 }
 */
 
-for (let i = 10; i >= 0; i--) {
+for (let i = 10; i <= 10; i++) {
     console.log(i)
-
+    --i
 }
 
--- EstruturaRepetição/estruturaFor/node_5d1d671faa619.tmp.js
+++ EstruturaRepetição/estruturaFor/node_5d1d671faa619.tmp.js
@@ -5,8 +5,7 @@ for (i = 0; i <= 10; i++) {
 }
 */
 
-for (let i = 10; i >= 0; i--) {
+for (let i = 10; i >= 0; i++) {
     console.log(i)
-
 }
 
